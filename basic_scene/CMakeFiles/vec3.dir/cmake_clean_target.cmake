80f509f67e2061029ae109df4c85253e107ca758
vpert <amorikov95@gmail.com>
2024-02-29 00:12:57 +0300

    second try

--- basic_scene/CMakeFiles/vec3.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libvec3.a"
-)
--- basic_scene/CMakeFiles/vec3.dir/vec3/vec3.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/vec3.dir/vec3/vec3.cpp.o: \
- /home/vpert/gitproj/raytracing/basic_scene/vec3/vec3.cpp \
- /usr/include/stdc-predef.h
